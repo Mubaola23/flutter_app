78c481fe021fa2081a34a9e21abd21e6edd684f9
Mubarak Shuaib <mubarakshuaib3@gmail.com>
2021-11-05 21:42:50 +0100

    Update

---
+++ android/app/src/main/kotlin/mubarak/flutterapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package mubarak.flutterapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/home_page.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            CircleAvatar(
+              radius: 50.0,
+            ),
+            SizedBox(
+              height: 16.0,
+            ),
+            Center(
+                child: Text(
+              "Name: Mubarak Shuaib Olasunkanmi",
+              style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.w500),
+            )),
+            SizedBox(
+              height: 16.0,
+            ),
+            Text(
+              "Job: Mobile App Developer",
+              style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.w500),
+            ),
+            SizedBox(
+              height: 16.0,
+            ),
+            ElevatedButton(onPressed: () {}, child: Text(" View Resume"))
+          ],
+        ),
+      ),
+    );
+  }
+}
